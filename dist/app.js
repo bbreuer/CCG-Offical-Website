ecc32d7c84fb97e03c9f2af55cdf9b971e81b241
bbreuer <bbreuer445@gmail.com>
2024-04-04 19:20:49 -0400

    ss

--- dist/app.js
+++ dist/app.js
@@ -17,17 +17,42 @@ const app = Vue.createApp({
             description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
           },
           {
-            name: "Serenmew",
-            imageUrl: "/seren.jpg",
-            games: ['Super Mario 64',], // Serenmew plays both games
+            name: "Silver",
+            imageUrl: "/silver.jpg",
+            games: ['Super Mario 64', 'Super Mario Odyssey'], // Serenmew plays both games
             socials: [
               { icon: "/twitch.svg", link: "#" },
               { icon: "/discord.svg", link: "#" },
               { icon: "/x-twitter.svg", link: "#" },
               { icon: "/youtube.svg", link: "#" },
             ],
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero placeat aliquam illo fugit laboriosam!",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
           },
+          {
+            name: "Tharoah",
+            imageUrl: "/tharoah.jpg",
+            games: ['Super Mario 64', 'Super Mario Odyssey'], // Serenmew plays both games
+            socials: [
+              { icon: "/twitch.svg", link: "#" },
+              { icon: "/discord.svg", link: "#" },
+              { icon: "/x-twitter.svg", link: "#" },
+              { icon: "/youtube.svg", link: "#" },
+            ],
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          },
+          {
+            name: "Animee",
+            imageUrl: "/anime.jpg",
+            games: ['Super Mario 64', 'Super Mario Odyssey'], // Serenmew plays both games
+            socials: [
+              { icon: "/twitch.svg", link: "#" },
+              { icon: "/discord.svg", link: "#" },
+              { icon: "/x-twitter.svg", link: "#" },
+              { icon: "/youtube.svg", link: "#" },
+            ],
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          },
+          
           
           // Add more player objects here, each with a 'games' field if applicable
         ],
