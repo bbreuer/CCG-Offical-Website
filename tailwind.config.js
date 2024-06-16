fdd69e8a933fd696e0776548c6818b0bcc6edb58
bbreuer <bbreuer445@gmail.com>
2024-06-15 22:44:46 -0700

    sasd

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-blue': '#1096CF',
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
